e5d9ca373da8d4c91b7612c3333afa0945f9a600
minabe <minabe@student.42tokyo.jp>
2023-09-12 15:42:59 +0900

    #2 fix: set_positionのx,yを逆にした

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 15:06:11 by minabe            #+#    #+#             */
-/*   Updated: 2023/09/12 15:26:24 by minabe           ###   ########.fr       */
+/*   Updated: 2023/09/12 15:38:53 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,18 +67,19 @@ void	set_field_of_view(t_player *player, double fov)
 {
 	set_vector(&player->dir, rotate_vec_x(player->dir, fov), rotate_vec_y(player->dir, fov));
 	set_vector(&player->plane, rotate_vec_x(player->plane, fov), rotate_vec_y(player->plane, fov));
+	printf("[direction]\ndegree: %f\n", atan(player->dir.y / player->dir.x) * 180 / M_PI);
 }
 
 void	set_position(t_game *game, int direction)
 {
 	if (direction == UP && check_nowall(game, direction))
-		game->player.pos.y++;
-	else if (direction == DOWN && check_nowall(game, direction))
-		game->player.pos.y--;
-	else if (direction == LEFT && check_nowall(game, direction))
 		game->player.pos.x--;
-	else if (direction == RIGHT && check_nowall(game, direction))
+	else if (direction == DOWN && check_nowall(game, direction))
 		game->player.pos.x++;
+	else if (direction == LEFT && check_nowall(game, direction))
+		game->player.pos.y--;
+	else if (direction == RIGHT && check_nowall(game, direction))
+		game->player.pos.y++;
 	printf("[pos]\nx: %f, y: %f\n", game->player.pos.x, game->player.pos.y);
 }
 
@@ -89,19 +90,19 @@ static bool	check_nowall(t_game *game, int direction)
 
 	if (direction == UP || direction == DOWN)
 	{
-		x = (int)game->player.pos.x;
-		if (direction == UP)
-			y = (int)game->player.pos.y + 1;
-		else
-			y = (int)game->player.pos.y - 1;
-	}
-	if (direction == LEFT || direction == RIGHT)
-	{
 		y = (int)game->player.pos.y;
-		if (direction == LEFT)
+		if (direction == UP)
 			x = (int)game->player.pos.x - 1;
 		else
 			x = (int)game->player.pos.x + 1;
+	}
+	if (direction == LEFT || direction == RIGHT)
+	{
+		x = (int)game->player.pos.x;
+		if (direction == LEFT)
+			y = (int)game->player.pos.y - 1;
+		else
+			y = (int)game->player.pos.y + 1;
 	}
 	if (game->map->map[x][y] == '1')
 		return (false);
