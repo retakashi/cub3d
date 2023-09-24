3301a00c717fbe12d95d2a5da1c72fd2bd4505cb
minabe <minabe@student.42tokyo.jp>
2023-09-24 15:24:36 +0900

    fix: 微修正

--- bonus/player/player_bonus.c
+++ bonus/player/player_bonus.c
@@ -6,7 +6,7 @@
 /*   By: minabe <minabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 15:06:11 by minabe            #+#    #+#             */
-/*   Updated: 2023/09/24 15:06:50 by minabe           ###   ########.fr       */
+/*   Updated: 2023/09/24 15:19:46 by minabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,40 +71,42 @@ void	set_field_of_view(t_player *player, double fov)
 
 void	set_position(t_game *game, int direction)
 {
-	if (direction == FRONT && check_nowall(game, FRONT))
-		set_vector(&game->player.pos, game->player.pos.x + game->player.dir.x * MOVE_SPEED, game->player.pos.y - game->player.dir.y * MOVE_SPEED);
-	else if (direction == BACK && check_nowall(game, BACK))
-		set_vector(&game->player.pos, game->player.pos.x - game->player.dir.x * MOVE_SPEED, game->player.pos.y + game->player.dir.y * MOVE_SPEED);
-	else if (direction == LEFT && check_nowall(game, LEFT))
-		set_vector(&game->player.pos, game->player.pos.x - game->player.dir.y * MOVE_SPEED, game->player.pos.y - game->player.dir.x * MOVE_SPEED);
-	else if (direction == RIGHT && check_nowall(game, RIGHT))
-		set_vector(&game->player.pos, game->player.pos.x + game->player.dir.y * MOVE_SPEED, game->player.pos.y + game->player.dir.x * MOVE_SPEED);
+	t_vector	new_pos;
+
+	new_pos = calculate_new_position(game, direction);
+	if (check_nowall(game, new_pos))
+		set_vector(&game->player.pos, new_pos.x, new_pos.y);
 }
 
-static bool	check_nowall(t_game *game, int direction)
+static t_vector	calculate_new_position(t_game *game, int direction)
 {
-	t_vector	pos;
+	t_vector	new_pos;
 
 	if (direction == FRONT)
 	{
-		pos.x = game->player.pos.x + game->player.dir.x * MOVE_SPEED;
-		pos.y = game->player.pos.y - game->player.dir.y * MOVE_SPEED;
+		new_pos.x = game->player.pos.x + game->player.dir.x * MOVE_SPEED;
+		new_pos.y = game->player.pos.y - game->player.dir.y * MOVE_SPEED;
 	}
 	if (direction == BACK)
 	{
-		pos.x = game->player.pos.x - game->player.dir.x * MOVE_SPEED;
-		pos.y = game->player.pos.y + game->player.dir.y * MOVE_SPEED;
+		new_pos.x = game->player.pos.x - game->player.dir.x * MOVE_SPEED;
+		new_pos.y = game->player.pos.y + game->player.dir.y * MOVE_SPEED;
 	}
 	if (direction == LEFT)
 	{
-		pos.x = game->player.pos.x - game->player.dir.y * MOVE_SPEED;
-		pos.y = game->player.pos.y - game->player.dir.x * MOVE_SPEED;
+		new_pos.x = game->player.pos.x - game->player.dir.y * MOVE_SPEED;
+		new_pos.y = game->player.pos.y - game->player.dir.x * MOVE_SPEED;
 	}
 	if (direction == RIGHT)
 	{
-		pos.x = game->player.pos.x + game->player.dir.y * MOVE_SPEED;
-		pos.y = game->player.pos.y + game->player.dir.x * MOVE_SPEED;
+		new_pos.x = game->player.pos.x + game->player.dir.y * MOVE_SPEED;
+		new_pos.y = game->player.pos.y + game->player.dir.x * MOVE_SPEED;
 	}
+	return (new_pos);
+}
+
+static bool	check_nowall(t_game *game, t_vector pos)
+{
 	if (game->map->map[(int)pos.y][(int)pos.x] == '1')
 		return (false);
 	return (true);
